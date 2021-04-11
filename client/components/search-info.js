b7d58996c5fe6a85d61d7380b1954d8717e4fea2
Egorius1979 <Egoriyvelikolepny1979@gmail.com>
2021-04-11 20:36:08 +0200

    fix some errors & logic

--- client/components/search-info.js
+++ client/components/search-info.js
@@ -1,30 +1,24 @@
 import React from 'react'
-import { Link, useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { setName } from '../redux/reducers/cryptocurrencies'
 
 const Info = () => {
-  const history = useHistory()
   const dispatch = useDispatch()
   const cryptoname = useSelector((store) => store.cryptos.cryptoname)
 
   return (
-    <div className="flex mb-20">
+    <form action={cryptoname} method="get" className="flex mb-20">
       <div>
         <input
           className="bg-white focus:outline-none border border-gray-300 py-2 px-4 block appearance-none leading-normal
-          placeholder-gray-700 placeholder-opacity-75 rounded-l-lg w-56"
+            placeholder-gray-700 placeholder-opacity-75 rounded-l-lg w-56"
           placeholder="Enter symbol"
           id="input-field"
           type="text"
           value={cryptoname}
           tabIndex="0"
           onChange={(e) => dispatch(setName(e.target.value))}
-          onKeyDown={({ key }) => {
-            if (key === 'Enter') {
-              history.push(cryptoname)
-            }
-          }}
         />
       </div>
       <div>
@@ -32,13 +26,13 @@ const Info = () => {
           <button
             className="bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 border border-blue-700 ml-2 rounded-r-lg"
             id="search-button"
-            type="button"
+            type="submit"
           >
             get info!
           </button>
         </Link>
       </div>
-    </div>
+    </form>
   )
 }
 
--- client/components/search-topcurrencies.js
+++ client/components/search-topcurrencies.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react'
-import { Link, useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const List = () => {
   const [valueNmb, setValueNmb] = useState('')
-  const history = useHistory()
 
   const topList = (e) => {
     if (!Number.isNaN(+e.target.value)) {
@@ -19,7 +18,7 @@ const List = () => {
   }
 
   return (
-    <div className="flex">
+    <form action={`list/${valueNmb}/1`} method="get" className="flex">
       <div>
         <input
           className="bg-white focus:outline-none border border-gray-300 py-2 px-4 block appearance-none leading-normal
@@ -29,11 +28,6 @@ const List = () => {
           type="text"
           value={valueNmb}
           onChange={topList}
-          onKeyDown={({ key }) => {
-            if (key === 'Enter') {
-              history.push(`list/${valueNmb}/1`)
-            }
-          }}
         />
       </div>
       <div>
@@ -41,13 +35,13 @@ const List = () => {
           <button
             className="bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 border border-blue-700 ml-2 rounded-r-lg"
             id="search-button"
-            type="button"
+            type="submit"
           >
             get info!
           </button>
         </Link>
       </div>
-    </div>
+    </form>
   )
 }
 
